2c6c753c3c1776dbd37e691fdd219a80b7626396
ConstantinWEB <94865176+ConstantinWEB@users.noreply.github.com>
2024-11-14 14:15:45 +0200

    FormaPage.java

--- src/test/java/org/example/pom/FormaPage.java
+++ src/test/java/org/example/pom/FormaPage.java
@@ -1,12 +1,16 @@
 package org.example.pom;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class FormaPage {
     @FindBy(xpath = "//*[@id='firstName']")
@@ -39,6 +43,18 @@ public class FormaPage {
     @FindBy(xpath = "//*[@id='currentAddress']")
     private WebElement currentAddressEl;
 
+    @FindBy(xpath = "//*[@id='react-select-3-input']")
+    private WebElement stateEl;
+
+    @FindBy(xpath = "//*[@id='react-select-4-input']")
+    private WebElement cityEl;
+
+    @FindBy(xpath = "//*[@id='uploadPicture']")
+    private WebElement uploadPictureEl;
+
+    @FindBy(xpath = "//*[@id='submit']")
+    private WebElement submitButtonEl;
+
     public static WebDriver driver;
 
     public FormaPage(WebDriver driver) {
@@ -66,23 +82,50 @@ public class FormaPage {
         mobileNumberEl.sendKeys(mobileNumber);
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
-        // Setează valoarea datei folosind JavaScript
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].value = arguments[1];", dateOfBirthEl, dateOfBirth);
+    public void setDateOfBirth() {
+        dateOfBirthEl.click();
+        // Selectăm anul 2005
+        driver.findElement(By.className("react-datepicker__year-select")).sendKeys("2005");
+        // Selectăm luna Iunie
+        driver.findElement(By.className("react-datepicker__month-select")).sendKeys("June");
+        // Selectăm ziua 1
+        driver.findElement(By.xpath("//div[contains(@class, 'react-datepicker__day') and text()='1']")).click();
     }
 
     public void setSubject(String subject) {
         subjectsInputEl.sendKeys(subject);
-        subjectsInputEl.sendKeys("\n"); // Press Enter to add the subject
+        subjectsInputEl.sendKeys("\n");
     }
 
     public void selectHobbySports() {
-        hobbySportsEl.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(hobbySportsEl)).click();
     }
 
     public void selectHobbyMusic() {
-        hobbyMusicEl.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(hobbyMusicEl)).click();
     }
 
+    public void setCurrentAddress(String address) {
+        currentAddressEl.sendKeys(address);
+    }
+
+    public void selectState(String state) {
+        stateEl.sendKeys(state);
+        stateEl.sendKeys(Keys.RETURN);
+    }
+
+    public void selectCity(String city) {
+        cityEl.sendKeys(city);
+        cityEl.sendKeys(Keys.RETURN);
+    }
+
+    public void uploadPicture(String filePath) {
+        uploadPictureEl.sendKeys(filePath);
+    }
+
+    public void submitForm() {
+        submitButtonEl.click();
+    }
 }
